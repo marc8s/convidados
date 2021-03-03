05e5cd412e8e1e5f3520fd0a54bb453631db1a5e
Marc8s <marcus.ecomp@gmail.com>
2021-03-03 22:59:36 +0000

    Padrão Singleton

--- app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -1,10 +1,27 @@
 package com.example.convidados.service.repository
 
+import android.content.Context
 import com.example.convidados.service.model.GuestModel
 
 //CRUD
+//singleton
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context){
+
+    private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    //para poder ser instanciada por GuestRepository em decorrencia do private constructor
+    companion object{
+        private lateinit var repository: GuestRepository
+        fun getInstance(context: Context): GuestRepository{
+
+            if(!::repository.isInitialized){
+                repository = GuestRepository(context)
+            }
+            return GuestRepository(context)
+        }
+    }
+
     fun getAll(): List<GuestModel>{
         val list: MutableList<GuestModel> = ArrayList()
         return list
