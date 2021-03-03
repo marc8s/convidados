e571dc4af746eb009afbb3a4dd709653d466f1b8
Marc8s <marcus.ecomp@gmail.com>
2021-03-03 22:01:14 +0000

    Cadastro - repositorio

--- app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
+++ app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
@@ -3,6 +3,8 @@ package com.example.convidados.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.convidados.viewmodel.GuestFormViewModel
 import com.example.convidados.R
@@ -19,16 +21,32 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
         mViewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
 
         setListeners()
+        observe()
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if(id == R.id.button_save){
+            val name = edit_name.text.toString()
+            val presence = radio_present.isChecked
+            mViewModel.save(name, presence)
+        }
+    }
+
+    private fun observe(){
+        mViewModel.saveGuest.observe(this, Observer {
+            if(it){
+                Toast.makeText(applicationContext, getString(R.string.sucesso), Toast.LENGTH_SHORT).show()
+
+            }else{
+                Toast.makeText(applicationContext, getString(R.string.falha), Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     private fun setListeners(){
         button_save.setOnClickListener(this)
     }
 
-    override fun onClick(v: View) {
-        val id = v.id
-        if(id == R.id.button_save){
 
-        }
-    }
 }
