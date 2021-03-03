126e186cbef04b4e8379cc9033f53a9550e3f4e3
Marc8s <marcus.ecomp@gmail.com>
2021-03-03 19:28:38 +0000

    Cadastro - layout, viewmodel e eventos

--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.convidados.ui.gallery
+package com.example.convidados.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,21 +9,22 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.convidados.R
+import com.example.convidados.viewmodel.AllGuestsViewModel
 
-class PresentFragment : Fragment() {
+class AllGuestsFragment : Fragment() {
 
-    private lateinit var presentViewModel: PresentViewModel
+    private lateinit var allGuestsViewModel: AllGuestsViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        presentViewModel =
-                ViewModelProvider(this).get(PresentViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        presentViewModel.text.observe(viewLifecycleOwner, Observer {
+        allGuestsViewModel =
+                ViewModelProvider(this).get(AllGuestsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        allGuestsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
